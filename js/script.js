b6064e46a6cf2b6e5ffbb7d861b78e9b339d9de5
evertonleonel <everton_leonel@hotmail.com>
2022-06-17 09:13:13 -0300

    Relogio Front End

---
+++ js/script.js
@@ -0,0 +1,28 @@
+let hours = document.querySelector('.hours');
+let minutes = document.querySelector('.minutes');
+let seconds = document.querySelector('.seconds');
+let ampm = document.querySelector('.ampm');
+
+
+let h = new Date().getHours();
+let m = new Date().getMinutes();
+let s = new Date().getSeconds();
+let am = h >= 12 ? 'PM' : 'AM';
+
+hours.innerHTML = h + '<span> :</span>';
+minutes.innerHTML = m + '<span> :</span>';
+seconds.innerHTML = s;
+ampm.innerHTML = am;
+
+
+// Converter 24h para 12h;
+
+if ( h > 12) {
+    hr = h - 12;
+}
+
+//Adicionar zero se tiver apenas 1 digito;
+
+h = ( h < 10 ) ? '0' +  h : h ;
+m = ( m < 10 ) ? '0' +  m : m ;
+s = ( s < 10 ) ? '0' +  s : s ;
